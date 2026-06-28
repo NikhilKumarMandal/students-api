1285944acdbac6264bc062153cc0311beb8adc02
Nikhil <kumarnikhil48578@gmail.com>
2026-06-28 23:09:15 +0530

    implement SQLite storage and update student ID type to int64

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/NikhilKumarMandal/students-api/internal/config"
 	"github.com/NikhilKumarMandal/students-api/internal/http/handlers/student"
+	"github.com/NikhilKumarMandal/students-api/internal/storage/sqlite"
 )
 
 
@@ -21,7 +22,15 @@ func main(){
 
 	cfg := config.MustLoad()
 
+	// database setup
 
+	_, err := sqlite.New(cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initialized",slog.String("env",cfg.Env), slog.String("version", "1.0.0"))
 
 	// setup router
 
@@ -56,7 +65,7 @@ func main(){
 	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
 	defer cancel()
 
-	err := server.Shutdown(ctx)
+	err = server.Shutdown(ctx)
 
 	if err != nil {
 		slog.Error("failed to shutdown server",slog.String("error",err.Error()))
